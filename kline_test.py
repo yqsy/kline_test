b10aac4d579711d7f1c359ac91ea94656f51c807
Guochun.Gong <yqsy021@126.com>
2017-07-23 15:14:36 +0800

    简单的布局demo

--- kline_test.py
+++ kline_test.py
@@ -3,7 +3,9 @@ import sys
 from PyQt5.QtCore import Qt
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QSplitter, \
-    QGridLayout, QGroupBox, QPushButton, QVBoxLayout, QGridLayout, QWidget
+    QGridLayout, QGroupBox, QPushButton, QVBoxLayout, QHBoxLayout, \
+    QGridLayout, QWidget, QBoxLayout, QLabel, QLineEdit, QCheckBox, \
+    QTreeWidget
 
 
 class MainWindow(QMainWindow):
@@ -13,43 +15,39 @@ class MainWindow(QMainWindow):
         self.resize(300, 300)
 
         widget = QWidget()
-
-        grid = QGridLayout()
-
-        grid.addWidget(self.get_buttons_box(), 0, 0)
-        grid.addWidget(self.get_buttons2_box(), 0, 1)
-        grid.addWidget(QPushButton('&退出'),1,1)
-        widget.setLayout(grid)
+        grid_layout = QGridLayout()
+        grid_layout.addWidget(self.get_left_box(), 0, 0)
+        grid_layout.addWidget(self.get_right_box(), 0, 1)
+        widget.setLayout(grid_layout)
         self.setCentralWidget(widget)
 
-    def get_buttons_box(self):
-        groupbox = QGroupBox()
+    def get_left_box(self):
+        box = QGroupBox('left')
 
-        box = QVBoxLayout()
-        box.addWidget(QPushButton('&1'))
-        box.addWidget(QPushButton('&2'))
-        box.addWidget(QPushButton('&3'))
-        box.addWidget(QPushButton('&4'))
-        box.addWidget(QPushButton('&5'))
-        box.addStretch(1)
+        grid_layout = QGridLayout()
+        grid_layout.addWidget(QLabel('label1'), 0, 0)
+        grid_layout.addWidget(QLineEdit(''), 0, 1)
+        grid_layout.addWidget(QLabel('label2'), 1, 0)
+        grid_layout.addWidget(QLineEdit(''), 1, 1)
+        grid_layout.addWidget(QCheckBox('check box'), 2, 0, 1, 2)
+        grid_layout.addWidget(QTreeWidget(), 3, 0, 1, 2)
+        grid_layout.addWidget(QLabel('label4'), 4, 0, 1, 2)
 
-        groupbox.setLayout(box)
+        box.setLayout(grid_layout)
+        return box
 
-        return groupbox
+    def get_right_box(self):
+        box = QGroupBox('right')
 
-    def get_buttons2_box(self):
-        groupbox = QGroupBox()
+        qbox_layout = QVBoxLayout()
+        qbox_layout.addWidget(QPushButton('button1'))
+        qbox_layout.addWidget(QPushButton('button2'))
+        qbox_layout.addWidget(QPushButton('button3'))
+        qbox_layout.addStretch(2)
+        qbox_layout.addWidget(QPushButton('button4'))
 
-        box = QVBoxLayout()
-        box.addWidget(QPushButton('&一'))
-        box.addWidget(QPushButton('&二'))
-        box.addWidget(QPushButton('&三'))
-        box.addWidget(QPushButton('&四'))
-        box.addWidget(QPushButton('&五'))
-        box.addStretch(1)
-        groupbox.setLayout(box)
-
-        return groupbox
+        box.setLayout(qbox_layout)
+        return box
 
 
 if __name__ == '__main__':
