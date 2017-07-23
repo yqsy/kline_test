a5b9f6050bc69e3cb2f98436be1d1f7304955e70
Guochun.Gong <yqsy021@126.com>
2017-07-23 13:41:56 +0800

    QSplitter 默认水平分割

--- kline_test.py
+++ kline_test.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSplitter
 
 
 class MainWindow(QMainWindow):
@@ -8,6 +8,11 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('kline_test')
         self.resize(870, 550)
 
+        # 默认水平分割
+        splitter_main = QSplitter()
+        splitter_vertical1 = QSplitter(splitter_main)
+        splitter_vertical2= QSplitter(splitter_main)
+        self.setCentralWidget(splitter_main)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
