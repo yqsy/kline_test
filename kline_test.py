b3279e074c6a1de3e91b52131debccb48d323d37
Guochun.Gong <yqsy021@126.com>
2017-07-23 13:25:09 +0800

    first commit

---
+++ kline_test.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('kline_test')
+        self.resize(870, 550)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
