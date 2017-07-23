dcccd55424d1686e7468051ffcd15c698f3791ed
Guochun.Gong <yqsy021@126.com>
2017-07-23 17:48:49 +0800

    画4条线

--- paint_test.py
+++ paint_test.py
@@ -24,27 +24,39 @@ class PaintArea(QWidget):
 
     def draw_horizontal_line(self):
         """画背景横线"""
-        painter = QPainter()
+        painter = QPainter(self)
         pen = QPen()
         pen.setColor(QColor('#FF0000'))
         painter.setPen(pen)
 
     def draw_block(self):
         """画背景方框"""
-        # self.width()
-        # self.height()
 
         margin_left = 80
         margin_right = 80
         margin_top = 20
         margin_bottom = 20
 
-        painter = QPainter()
+        painter = QPainter(self)
         pen = QPen()
-        pen.setColor(QColor='#FF0000')
+        pen.setColor(QColor('#FF0000'))
         painter.setPen(pen)
-        painter.drawLine(margin_left, self.height() - margin_top,
-                         margin_left, margin_bottom)
+
+        # 4个角的坐标 左上 左下 右上 右下
+        points = [
+            QPoint(margin_left, self.height() - margin_top),
+            QPoint(margin_left, margin_bottom),
+            QPoint(self.width() - margin_right, self.height() - margin_top),
+            QPoint(self.width() - margin_right, margin_bottom)
+        ]
+
+        upper_left, lower_left, upper_right, lower_right = range(4)
+
+        painter.drawLine(points[upper_left], points[lower_left])
+        painter.drawLine(points[upper_left], points[upper_right])
+
+        painter.drawLine(points[lower_right], points[lower_left])
+        painter.drawLine(points[lower_right], points[upper_right])
 
 
 class Window(QMainWindow):
